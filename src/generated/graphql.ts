ba160b67a1230db30a61e76129e13e566ac935b9
Elfi Yang <elfiyang16@gmail.com>
2021-04-03 17:01:52 +0100

    feat: add codegen

---
+++ src/generated/graphql.ts
@@ -0,0 +1,207 @@
+import { gql } from '@apollo/client';
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  Date: any;
+  GreaterThan: any;
+  /** The `Upload` scalar type represents a file upload. */
+  Upload: any;
+};
+
+
+
+
+
+
+
+
+export type Capsule = {
+  __typename?: 'Capsule';
+  id?: Maybe<Scalars['ID']>;
+  landings?: Maybe<Scalars['Int']>;
+  missions?: Maybe<Array<Maybe<CapsuleMission>>>;
+  original_launch?: Maybe<Scalars['String']>;
+  reuse_count?: Maybe<Scalars['Int']>;
+  status?: Maybe<Scalars['String']>;
+  type?: Maybe<Scalars['String']>;
+  dragon?: Maybe<Dragon>;
+  /**
+   * Unique ID
+   * @deprecated No idea, guess why
+   */
+  uid?: Maybe<Scalars['ID']>;
+};
+
+export type CapsulesFind = {
+  id?: Maybe<Scalars['ID']>;
+  landings?: Maybe<Scalars['Int']>;
+  mission?: Maybe<Scalars['String']>;
+  original_launch?: Maybe<Scalars['Date']>;
+  reuse_count?: Maybe<Scalars['Int']>;
+  status?: Maybe<Scalars['String']>;
+  type?: Maybe<Scalars['String']>;
+};
+
+export type CapsuleMission = {
+  __typename?: 'CapsuleMission';
+  flight?: Maybe<Scalars['Int']>;
+  name?: Maybe<Scalars['String']>;
+};
+
+export type Mission = {
+  __typename?: 'Mission';
+  description?: Maybe<Scalars['String']>;
+  id?: Maybe<Scalars['ID']>;
+  manufacturers?: Maybe<Array<Maybe<Scalars['String']>>>;
+  name?: Maybe<Scalars['String']>;
+  twitter?: Maybe<Scalars['String']>;
+  website?: Maybe<Scalars['String']>;
+  wikipedia?: Maybe<Scalars['String']>;
+};
+
+export type MissionsFind = {
+  id?: Maybe<Scalars['ID']>;
+  manufacturer?: Maybe<Scalars['String']>;
+  name?: Maybe<Scalars['String']>;
+  payload_id?: Maybe<Scalars['String']>;
+};
+
+
+export type Result = {
+  __typename?: 'Result';
+  totalCount?: Maybe<Scalars['Int']>;
+};
+
+export type Address = {
+  __typename?: 'Address';
+  address?: Maybe<Scalars['String']>;
+  city?: Maybe<Scalars['String']>;
+  state?: Maybe<Scalars['String']>;
+};
+
+export type Distance = {
+  __typename?: 'Distance';
+  feet?: Maybe<Scalars['Float']>;
+  meters?: Maybe<Scalars['Float']>;
+};
+
+export type Force = {
+  __typename?: 'Force';
+  kN?: Maybe<Scalars['Float']>;
+  lbf?: Maybe<Scalars['Float']>;
+};
+
+export type Link = {
+  __typename?: 'Link';
+  article?: Maybe<Scalars['String']>;
+  reddit?: Maybe<Scalars['String']>;
+  wikipedia?: Maybe<Scalars['String']>;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  capsules?: Maybe<Array<Maybe<Capsule>>>;
+  capsule?: Maybe<Capsule>;
+  missions?: Maybe<Array<Maybe<Mission>>>;
+  mission?: Maybe<Mission>;
+  dragons?: Maybe<Array<Maybe<Dragon>>>;
+  dragon?: Maybe<Dragon>;
+};
+
+
+export type QueryCapsulesArgs = {
+  find?: Maybe<CapsulesFind>;
+  limit?: Maybe<Scalars['Int']>;
+  offset?: Maybe<Scalars['Int']>;
+  order?: Maybe<Scalars['String']>;
+  sort?: Maybe<Scalars['String']>;
+};
+
+
+export type QueryCapsuleArgs = {
+  id: Scalars['ID'];
+};
+
+
+export type QueryMissionsArgs = {
+  find?: Maybe<MissionsFind>;
+  limit?: Maybe<Scalars['Int']>;
+  offset?: Maybe<Scalars['Int']>;
+};
+
+
+export type QueryMissionArgs = {
+  id: Scalars['ID'];
+};
+
+
+export type QueryDragonsArgs = {
+  limit?: Maybe<Scalars['Int']>;
+  offset?: Maybe<Scalars['Int']>;
+};
+
+
+export type QueryDragonArgs = {
+  id: Scalars['ID'];
+};
+
+export type Mass = {
+  __typename?: 'Mass';
+  kg?: Maybe<Scalars['Int']>;
+  lb?: Maybe<Scalars['Int']>;
+};
+
+export type Volume = {
+  __typename?: 'Volume';
+  cubic_feet?: Maybe<Scalars['Int']>;
+  cubic_meters?: Maybe<Scalars['Int']>;
+};
+
+export type Dragon = {
+  __typename?: 'Dragon';
+  active?: Maybe<Scalars['Boolean']>;
+  crew_capacity?: Maybe<Scalars['Int']>;
+  description?: Maybe<Scalars['String']>;
+  diameter?: Maybe<Distance>;
+  dry_mass_kg?: Maybe<Scalars['Int']>;
+  dry_mass_lb?: Maybe<Scalars['Int']>;
+  first_flight?: Maybe<Scalars['String']>;
+  height_w_trunk?: Maybe<Distance>;
+  id?: Maybe<Scalars['ID']>;
+  launch_payload_mass?: Maybe<Mass>;
+  launch_payload_vol?: Maybe<Volume>;
+  name?: Maybe<Scalars['String']>;
+  orbit_duration_yr?: Maybe<Scalars['Int']>;
+  return_payload_mass?: Maybe<Mass>;
+  return_payload_vol?: Maybe<Volume>;
+  sidewall_angle_deg?: Maybe<Scalars['Float']>;
+  type?: Maybe<Scalars['String']>;
+  wikipedia?: Maybe<Scalars['String']>;
+  /**
+   * Unique ID
+   * @deprecated No idea, guess why
+   */
+  uid?: Maybe<Scalars['ID']>;
+};
+
+export enum CacheControlScope {
+  Public = 'PUBLIC',
+  Private = 'PRIVATE'
+}
+
+
+export type Location = {
+  __typename?: 'Location';
+  latitude?: Maybe<Scalars['Float']>;
+  longitude?: Maybe<Scalars['Float']>;
+  name?: Maybe<Scalars['String']>;
+  region?: Maybe<Scalars['String']>;
+};
